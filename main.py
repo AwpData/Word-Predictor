cc91fa0800b77da694f0c4b30b4867fed2a1aa2d
Trevor Tang <awpdata@gmail.com>
2021-08-15 18:09:09 -1000

    Added tail counter for bigram model

--- main.py
+++ main.py
@@ -13,32 +13,27 @@ while True:
         print("File found!")
         break
 
-unique_words = set()
 bigrams = list()
 words = WhitespaceTokenizer().tokenize(file.read())
-for word in words:
-    unique_words.add(word)
 for i in range(0, len(words) - 1):
     bigrams.append([words[i], words[i + 1]])
 print("Corpus statistics")
 print("All tokens: " + str(len(words)))
-print("Unique tokens: " + str(len(unique_words)))
-print("Number of bigrams: " + str(len(bigrams)))
-
+print("\nEnter a word and see how many words follow it in the corpus ('exit' to quit): ")
 while True:
-    try:
-        index = str(input())
-        if index == "exit":
-            break
-        index = int(index)
-    except ValueError or TypeError:
-        print("Type Error. Please input an integer")
+    word = str(input())
+    if word == "exit":
+        break
     else:
-        try:
-            print(words[index])
-            if index < 0:
-                print("Head: {}\tTail: {}".format(words[index - 1], words[index]))
-            else:
-                print("Head: {}\tTail: {}".format(words[index], words[index + 1]))
-        except IndexError:
-            print("Index Error. Please input an integer that is in the range of the corpus.")
+        counter_dict = dict()
+        for words in bigrams:
+            if words[0] == word:
+                counter_dict.setdefault(words[1], 0)
+                counter_dict[words[1]] += 1
+        if len(counter_dict) == 0:
+            print("The requested word is not in the model. Please input another word.")
+            continue
+        counter_dict = dict(sorted(counter_dict.items(), key=lambda item: item[1], reverse=True))
+        print("\nHead: {}".format(word))
+        for key, value in counter_dict.items():
+            print("Tail: {}\tCount: {}".format(key, value))
