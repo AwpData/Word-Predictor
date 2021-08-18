47d6ebc28cdc1f069373b1ab2063ec4669a1c4d8
Trevor Tang <awpdata@gmail.com>
2021-08-17 22:18:02 -0700

    Sentences make more sense with tigam modeling

--- main.py
+++ main.py
@@ -15,48 +15,29 @@ while True:  # First, get the corpus text (and make sure it exists)
     else:
         break
 
-bigrams = list()
+tigrams = list()
 words = WhitespaceTokenizer().tokenize(file.read())
-for i in range(0, len(words) - 1):  # Then, tokenize (by whitespace) the text into pairs (head and tail) and add to list
-    bigrams.append([words[i], words[i + 1]])
+for i in range(0, len(words) - 2):  # Then, tokenize (by whitespace) the text into pairs (head and tail) and add to list
+    tigrams.append([words[i] + " " + words[i + 1], words[i + 2]])
 
 print("\n")
 for _ in range(0, 10):  # Loop for 10 sentences
     while True:  # Make sure the head word starts with a capital and has no period
-        random_word = random.choice(bigrams)[0]
-        if re.match(r"[A-Z][A-Za-z']*$", random_word) is not None:
+        random_word = random.choice(tigrams)[0]
+        if re.match(r"[A-Z][\w\s']*$", random_word) is not None:
             sentence = random_word
             break
     while True:  # Loop for at least 5 words
-        counter_dict = dict()
-        for words in bigrams:  # Gets all possible tails for head word, and then count them in dictionary
+        counter_d = dict()
+        for words in tigrams:  # Gets all possible tails for head word, and then count them in dictionary
             if words[0] == random_word:
-                counter_dict.setdefault(words[1], 0)
-                counter_dict[words[1]] += 1
-        random_tail = random.choices(list(counter_dict.keys()), weights=[nums * 10 for nums in counter_dict.values()])[
-            0]  # Get the random tail based on probability (weight heavier towards more common words by counter)
+                counter_d.setdefault(words[1], 0)
+                counter_d[words[1]] += 1
+        # Get the random tail based on probability (weight heavier towards more common words by counter)
+        random_tail = random.choices(list(counter_d.keys()), weights=[nums * 10 for nums in counter_d.values()])[0]
         sentence += " " + random_tail  # Add to our sentence the valid tail
         if re.match(r"[\w']+[.?!]+$", random_tail) is not None:  # If random_tail contains end-punctuation
             if len(sentence.split()) >= 5:  # This is a valid sentence if word length >= 5, so break!
                 break
-        random_word = random_tail  # All else, the new head is the last tail
+        random_word = random_word.split()[1] + " " + random_tail  # All else, the new head is the last tail
     print(sentence)  # Finally, print the sentence
-
-print("\nEnter one word at a time now to see the probability of it being chosen ('0' = quit)")
-while True:
-    word = str(input())
-    if word == "0":
-        break
-    else:
-        counter_dict = dict()
-        for words in bigrams:
-            if words[0] == word:
-                counter_dict.setdefault(words[1], 0)
-                counter_dict[words[1]] += 1
-        if len(counter_dict) == 0:
-            print("The requested word is not in the model. Please input another word.")
-            continue
-        counter_dict = dict(sorted(counter_dict.items(), key=lambda item: item[1], reverse=True))
-        print("\nHead: {}".format(word))
-        for key, value in counter_dict.items():
-            print("Tail: {}\tCount: {}".format(key, value))
